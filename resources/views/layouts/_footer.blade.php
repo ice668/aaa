6dd85132d2d5d8ff8a257057ffd503a89909dab4
ice <1355871239@qq.com>
2020-02-20 04:33:33 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="https://learnku.com/laravel/courses" target=_blank>
+    刻意练习，每日精进你觉得呢？？？
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >关于懂么？</a>
+  </div>
+</footer>
