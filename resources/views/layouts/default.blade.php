0d9578f9a2e4ab8f94fa662b28d7159ad450dbae
ice <1355871239@qq.com>
2020-02-20 04:11:38 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <title>@yield('title', 'Weibo App') - 百度一下头给你打烂</title>
-    <link rel="stylesheet" href="/css/app.css">
+    
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
