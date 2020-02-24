396d21d25e076b5b9857bd6a2c1ee59617f6103d
ice <1355871239@qq.com>
2020-02-24 12:52:23 +0000

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+class SessionsController extends Controller
+{
+     public function create()
+    {
+        return view('sessions.create');
+    }
+
+//我们可以使用 Illuminate\Http\Request 实例来接收用户的所有输入数据，当我们需要取出 Request 实例的单个值时，可以使用以下方法
+    public function store(Request $request)
+    {//资格验证
+       $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+           'password' => 'required'
+       ]);
+
+        if (Auth::attempt($credentials)) {
+        	 session()->flash('success', '欢迎回来！');
+        	 return redirect()->route('users.show', [Auth::user()]);
+           // 重新调配Auth::user() 方法来获取 当前登录用户 的信息，并将数据传送给路由
+       } else {
+       		 session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           // 登录失败后的相关操作
+       		 return redirect()->back()->withInput();
+       	//使用 withInput() 后模板里 old('email') 将能获取到上一次用户提交的内容，这样用户就无需再次输入邮箱等内容
+       }
+       return;
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,6 @@ Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');*/
 /*新增的 resource 方法将遵从 RESTful 架构为用户资源生成路由。该方法接收两个参数，第一个参数为资源名称，第二个参数为控制器名称*/
 Route::resource('users', 'UsersController');
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
