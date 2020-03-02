7f2331e1800025aed2e03b8b67c895b484581724
ice <1355871239@qq.com>
2020-03-02 07:05:31 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,17 +1,31 @@
 @extends('layouts.default')
-@section('title', 'section主页')
 
 @section('content')
-  <div class="jumbotron">
-  	<h1>whats 你愁啥？？？？？</h1>
-  	<p class="lead">不要点这里 <a href="https://learnku.com/courses/laravel-essential-training">？？</a>可以嘛</p>
-
-  	 <p>
-      一切，都是浮云。
-    </p>
-
-    <p>
-      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">注册个jiji？</a>
-    </p>
-  </div>
+  @if (Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+    <div class="jumbotron">
+      <h1>Hello Laravel</h1>
+      <p class="lead">
+        你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
+      </p>
+      <p>
+        一切，将从这里开始。
+      </p>
+      <p>
+        <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+      </p>
+    </div>
+  @endif
 @stop
